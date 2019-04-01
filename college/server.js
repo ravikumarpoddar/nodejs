982db4b1649fd67176c7db51d5e5579f293ccccb
ravikumarpoddar <ravikumar.ac.in@gmail.com>
2019-04-01 16:12:32 +0530

    my nodejs restful APIs

--- college/server.js
+++
@@ -1,9 +0,0 @@
-const http = require('http');
-const app = require('./app');
-const server = http.createServer(app);
-
-const port = process.env.PORT || 6000;
-
-server.listen(port, ()=>{
-    console.log("Server is Running");
-});
--- login/model/model.go
+++
@@ -1,7 +0,0 @@
-package model
-
-type Logger struct {
-	ID       int    `json:"id"`
-	Email    string `json:"email"`
-	Password string `json:"password"`
-}
